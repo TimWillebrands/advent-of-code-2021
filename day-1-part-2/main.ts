50c9dc9a3f6b27f8fdf1c296ab93c903c047b076
Tim Willebrands <mail@timwillebrands.nl>
2021-12-01 18:31:53 +0100

    oh deer

--- day-1-part-2/main.ts
+++ day-1-part-2/main.ts
@@ -3,8 +3,6 @@ const answer = input
     .split('\n')
     .map(line => Number.parseInt(line))
     .map((current, index, array) => current + (array[index + 1]||0) + (array[index + 2]||0))
-    // .splice(0,10)
-    // .forEach(val => console.log(val))
     .reduce((acc, current, index, array) => !Number.isNaN(current) && current - array[index+1] < 0 ? ++acc : acc, 0)
 
 console.log(answer)
