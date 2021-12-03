e45620025a76257dfa8138a3fc176a896161e5cf
Tim0theus <Tim.Willebrands@gmail.com>
2021-12-03 15:48:48 +0100

    day 3 ts is klaar

--- day-3/ts/part1.ts
+++ day-3/ts/part1.ts
@@ -1,9 +1,3 @@
-// const movement = Deno.args[0]
-//     .split('\n')
-//     .reduce((agg, current) => current.forEach, [0,0,0,0,0,0,0,0,0,0,0,0])
-
-// console.log(movement[0] * movement[1])
-
 const meestVoorkomend = [...Deno.args[0]]
     .filter(char => char === '1' || char === '0')
     .map(char => Number.parseInt(char))
@@ -11,4 +5,7 @@ const meestVoorkomend = [...Deno.args[0]]
     .map(totals => totals > 500 ? '1' : '0')
     .join('')
 
-console.log(meestVoorkomend)
+const gamma = Number.parseInt(meestVoorkomend, 2);
+const epsil = (~Number.parseInt(meestVoorkomend, 2)) ^ 4294963200
+
+console.log(meestVoorkomend, gamma, epsil, gamma * epsil)
