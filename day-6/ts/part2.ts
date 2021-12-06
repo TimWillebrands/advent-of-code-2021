6ad0131643e44ea90ac5d5e234cba96e3f58e6d5
Tim Willebrands <mail@timwillebrands.nl>
2021-12-06 21:10:59 +0100

    Day 6 part 1

---
+++ day-6/ts/part2.ts
@@ -0,0 +1,19 @@
+// const input = Deno.args[0]
+//     .split(',')
+//     .map(char => Number.parseInt(char))
+
+// let mutInput = [...input];
+
+// for (let i = 0; i <= 256; ++i){
+//     const newFishies = mutInput.map(fish => --fish);
+
+//     newFishies
+//         .filter(fish => fish < 0)
+//         .forEach(_ => newFishies.push(8))
+
+//     const finalFishies = newFishies
+//         .map(fish => fish < 0 ? 6 : fish)
+
+//     console.log(`After ${i} days:\t${mutInput.length} fishies`)
+//     mutInput = finalFishies;
+// }
